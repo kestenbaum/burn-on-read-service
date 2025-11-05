20838bb15539e3b47bb1fdb8bcddcd56e64a1043
Oleksii <kespa71for@gmail.com>
2025-11-05 11:52:53 +0100

    chore: init nunjucks

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import nunjucks from "nunjucks";
+import path from "path";
 import { Request, Response } from "express";
 
 const app = express()
+const viewsPath = path.join(__dirname, 'view');
+
+nunjucks.configure(viewsPath, {
+    autoescape: true,
+    express: app,
+    watch: true,
+})
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Test")
+    res.render("index.html")
 })
 
 app.listen(3000, "0.0.0.0", () => {

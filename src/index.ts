20ab1452c64f92fa74368f48123917c0492a8cbd
Oleksii <kespa71for@gmail.com>
2025-11-05 14:50:10 +0100

    feat: add input handler

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import nunjucks from "nunjucks";
 import path from "path";
-import { Request, Response } from "express";
 
-const app = express()
+import type { Request, Response } from "express";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 const viewsPath = path.join(__dirname, 'view');
 
 nunjucks.configure(viewsPath, {
@@ -14,6 +16,17 @@ nunjucks.configure(viewsPath, {
 
 app.get("/", (req: Request, res: Response) => {
     res.render("index.html")
+})
+
+app.post("/create", (req: Request, res: Response) => {
+    const input = req.body.text;
+    const message = "temporary-id";
+
+    const shareLink = `http://${req.get('host')}/message/${message}`;
+
+    res.render('link.html', { 
+        shareLink,
+    });
 })
 
 app.listen(3000, "0.0.0.0", () => {

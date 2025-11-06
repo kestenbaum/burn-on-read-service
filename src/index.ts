e3d51efaa1c42bf6fac140905d1c537c10fdcde4
Oleksii <kespa71for@gmail.com>
2025-11-06 11:06:15 +0100

    feat: add function to print and deleted links

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { v4 } from "uuid";
 import { burnOnReadMiddleware } from "./middlewares/burnOnReadMiddleware";
 import { BurnRequest, messages } from "./types";
 import type { Request, Response } from "express";
+import { promises as fs} from "fs";
+
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -12,9 +14,7 @@ const app = express();
 const PORT = Number(process.env.PORT) || 3001
 const viewsPath = path.join(process.cwd(), 'src/view');
 
-
 app.use(express.urlencoded({ extended: true }));
-
 
 nunjucks.configure(viewsPath, {
     autoescape: true,
@@ -22,9 +22,10 @@ nunjucks.configure(viewsPath, {
     watch: true,
 })
 
-app.get("/", (req: Request, res: Response) => {
-    res.render("index.html")
+app.get("/", async (req: Request, res: Response) => {
+    await res.render("index.html")
 })
+
 
 app.post("/create", (req: Request, res: Response) => {
     const userInput = req.body.message;
@@ -32,17 +33,20 @@ app.post("/create", (req: Request, res: Response) => {
     messages[messageId] = userInput.trim();
 
     const shareLink = `${req.protocol}://${req.get('host')}/message/${messageId}`;
+    fs.writeFile("example.txt", shareLink);
     res.render('link.html', { shareLink });
+    console.log(`This is a userInput: ${userInput}`);
 })
 
 app.get('/message/:id', 
     burnOnReadMiddleware,
-    (req: BurnRequest, res: Response) => {
+    (req: Request, res: Response) => {
         res.render('read.html', {
-            content: req.messageContent
+           
         });
     }
 );
+
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log("server listen")
--- src/middlewares/burnOnReadMiddleware.ts
+++ src/middlewares/burnOnReadMiddleware.ts
@@ -1,18 +1,16 @@
-// src/middleware/BurnOnReadMiddleware.ts
-import { Response, NextFunction } from 'express';
-import { BurnRequest, messages } from '../types'; 
+import fs from 'fs';
+import path from 'path';
+import { Request, Response, NextFunction } from 'express';
 
-export const burnOnReadMiddleware = (
-    req: BurnRequest, 
-    res: Response, 
-    next: NextFunction
-) => {
-    const messageId = req.params.id; 
-    const messageContent = messages[messageId]; 
+export const burnOnReadMiddleware =  (req: Request, res: Response, next: NextFunction) => {
+  const filePath = path.join(process.cwd(), 'example.txt');
 
-    if (messageContent) {
-        delete messages[messageId]
-        req.messageContent = messageContent; 
-        next(); 
-    } 
-};
+  if (fs.existsSync(filePath)) {
+    fs.unlink(filePath, (err) => {
+      if (err) console.error('Error:', err);
+      else console.log('example.txt deleted');
+    });
+  }
+
+  next();
+}
